081fbca187c6b5129bde49b507aad3cee78f9cd6
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-08-28 15:13:13 +0700

    1. fixed units button 2. fixed search bar and logo in the screen that is larger than 768px

--- src/components/weather-information/index.tsx
+++ src/components/weather-information/index.tsx
@@ -3,10 +3,25 @@ import React from "react";
 import WeatherForecast from "../weather-forecast";
 import PrecipitationForecast from "../precipitation-forecast";
 import MoreInformation from "../more-infomation";
+import SearchBar from "../search-bar";
 
 const WeatherInformation = () => {
   return (
     <div className="weather-information overflow-y-scroll bg-white p-4 py-9 xs:p-9">
+      <div className="w-full max-md:hidden">
+        <nav className="flex w-full justify-between gap-4">
+          <div className="flex items-center justify-between">
+            <span className="select-none text-4xl font-bold text-primary-base">
+              Clymatic
+            </span>
+          </div>
+
+          <div>
+            <SearchBar />
+          </div>
+        </nav>
+      </div>
+
       <div className="flex max-w-full flex-col gap-16">
         <div className="forecast-container">
           <WeatherForecast title="Today's Forecast" />
