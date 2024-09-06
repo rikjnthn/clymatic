1a9dee681cea173d2652489ab3375a2605c62d48
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-09-06 10:07:54 +0700

    1. move suspend up to wrap weather page component 2. prevent submit search when no user input

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -11,20 +11,23 @@ const SearchBar = () => {
   const pathname = usePathname();
 
   const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!search.trim()) return;
+
     const searchParams = new URLSearchParams();
     searchParams.set("city", encodeURIComponent(search));
 
     router.push(`${pathname}?${searchParams.toString()}`);
 
     setSearch("");
-
-    e.preventDefault();
   };
 
   return (
     <form
       onSubmit={handleSearch}
       className="flex w-full max-w-sm items-center overflow-clip rounded-full border border-dark-gray bg-white pl-4 pr-2.5 max-md:py-0.5 md:max-w-xs"
+      role="form"
     >
       <input
         onChange={(e) => setSearch(e.currentTarget.value)}
